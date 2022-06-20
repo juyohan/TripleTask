c8e0e518eb76711283952dcf3578a0cf1c26d68c
juyohan <dygks8556@naver.com>
2022-06-20 22:49:53 +0900

    deleteBy -> delete 메소드로 변경

--- src/main/kotlin/com/example/triple/service/common/ReviewCommonService.kt
+++ src/main/kotlin/com/example/triple/service/common/ReviewCommonService.kt
@@ -13,22 +13,42 @@ class ReviewCommonService(
 
     val reviewRepository: ReviewRepository,
 ) {
+    /**
+     * placeId 의 장소에 리뷰가 몇개 달렸는지 갯수를 반환
+     *
+     * @param placeId 장소의 id 값
+     *
+     * @return 해당 장소를 가지고 있는 컬럼의 개수
+     */
     fun countReviewByPlaceId(placeId: String): Int = reviewRepository.countByPlaceId(placeId)
 
+    /**
+     * ReviewDto 를 통해 Reivewer 작성자의 Entity 에 접근한 뒤, DB에 저장
+     *
+     * @param reviewDto 리뷰의 정보를 담고있는 DTO 객체
+     */
     fun addReviewDto(reviewDto: ReviewDto) {
         var review: Review = reviewDto.toEntity()
         review.reviewer = reviewerCommonService.getReferenceReviewer(reviewDto.reviewerId)
         reviewRepository.save(review)
     }
 
+    /**
+     *
+     */
     fun findOneReview(reviewId: String): ReviewDto = reviewRepository.findByReviewId(reviewId)
-        .orElseThrow { throw BusinessException(Exceptions.REVIEW_NOT_FOUND)}.toDto()
+        .orElseThrow { throw BusinessException(Exceptions.REVIEW_NOT_FOUND) }.toDto()
 
+    //review Entity 에 접근하여 데이터를 변경
     fun updateOneReview(reviewDto: ReviewDto) {
         val review = reviewRepository.getReferenceById(reviewDto.id)
         review.photoCount = reviewDto.photoCount
         review.contentLength = reviewDto.contentLength
     }
 
-    fun deleteOneReview(reviewId: String) = reviewRepository.deleteByReviewId(reviewId)
+    // Review 프록시 Entity 반환
+    fun getReferenceReview(id: Long): Review = reviewRepository.getReferenceById(id)
+
+    // 데이터를 삭제하는 메소드
+    fun deleteOneReview(id: Long) = reviewRepository.delete(getReferenceReview(id))
 }
