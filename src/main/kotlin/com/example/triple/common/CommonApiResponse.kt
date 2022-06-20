76c02ecbd329ff8d40dcda5c3582612a6de555c3
juyohan <dygks8556@naver.com>
2022-06-20 22:48:29 +0900

    성공적으로 마쳤을 때, 응답 처리

--- src/main/kotlin/com/example/triple/common/CommonApiResponse.kt
+++ src/main/kotlin/com/example/triple/common/CommonApiResponse.kt
@@ -1,8 +1,17 @@
 package com.example.triple.common
 
-data class CommonApiResponse(
-    val userId: String = "",
-    val points: Long = 0L,
-) {
+import com.example.triple.dto.ErrorDto
 
+data class CommonApiResponse(
+    val code: String = "",
+    val message: String = "",
+    val data: Any,
+) {
+    companion object {
+        fun success(data: Any): CommonApiResponse =
+            CommonApiResponse(code = "Success", message = "성공했습니다.", data = data)
+
+        fun error(errorDto: ErrorDto): CommonApiResponse =
+            CommonApiResponse(code = errorDto.errorCode, message = errorDto.message, data = errorDto)
+    }
 }
--- src/main/kotlin/com/example/triple/controller/ReviewEventController.kt
+++ src/main/kotlin/com/example/triple/controller/ReviewEventController.kt
@@ -1,5 +1,7 @@
 package com.example.triple.controller
 
+import com.example.triple.common.CommonApiResponse
+import com.example.triple.common.CommonApiResponse.Companion.success
 import com.example.triple.dto.EventDto
 import com.example.triple.exception.BusinessException
 import com.example.triple.exception.Exceptions
@@ -7,10 +9,15 @@ import com.example.triple.model.enumeration.ReviewActionType
 import com.example.triple.service.ReviewEventDeleteService
 import com.example.triple.service.ReviewEventModifyService
 import com.example.triple.service.ReviewEventRegisterService
+import com.example.triple.service.common.ReviewerCommonService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+
+import org.springframework.http.HttpStatus.OK
 
 @RestController
 @RequestMapping("/events")
@@ -18,22 +25,25 @@ class ReviewEventController(
     val reviewEventRegisterService: ReviewEventRegisterService,
     val reviewEventDeleteService: ReviewEventDeleteService,
     val reviewEventModifyService: ReviewEventModifyService,
+    val reviewerCommonService: ReviewerCommonService,
 ) {
 
     @PostMapping
     fun saveEvent(
         eventDto: EventDto
-    ) {
-        when(eventDto.action) {
-            ReviewActionType.ADD -> reviewEventRegisterService.addReviewEvent(eventDto)
-            ReviewActionType.DELETE -> reviewEventDeleteService.deleteEvent(eventDto)
-            ReviewActionType.MOD -> reviewEventModifyService.modifyEvent(eventDto)
-            else -> throw BusinessException(Exceptions.NO_ACTION_TYPE)
-        }
-    }
+    ): ResponseEntity<CommonApiResponse> =
+        ResponseEntity(
+            success(
+                when (eventDto.action) {
+                    ReviewActionType.ADD -> reviewEventRegisterService.addReviewEvent(eventDto)
+                    ReviewActionType.DELETE -> reviewEventDeleteService.deleteEvent(eventDto)
+                    ReviewActionType.MOD -> reviewEventModifyService.modifyEvent(eventDto)
+                    else -> throw BusinessException(Exceptions.NO_ACTION_TYPE)
+                }
+            ), OK
+        )
 
     @GetMapping
-    fun getOneUserPoint() {
-
-    }
+    fun getReviewer(@RequestParam userId: String): ResponseEntity<CommonApiResponse>
+    = ResponseEntity(success(reviewerCommonService.findOneReviewer(userId)), OK)
 }
