fe81988a70c91ae244974c35ca69762836463c0c
juyohan <dygks8556@naver.com>
2022-06-23 16:25:29 +0900

    operator 함수 제거

--- src/main/kotlin/com/example/triple/model/enumeration/OperatorType.kt
+++ src/main/kotlin/com/example/triple/model/enumeration/OperatorType.kt
@@ -1,17 +1,8 @@
 package com.example.triple.model.enumeration
 
-import com.example.triple.dto.ReviewerDto
-
 enum class OperatorType(
     var value: Int = 0
 ) {
     PLUS(1),
     MINUS(-1);
-
-    companion object {
-        fun operator(reviewerDto: ReviewerDto, type: OperatorType): OperatorType {
-            reviewerDto.point += type.value
-            return type
-        }
-    }
 }
